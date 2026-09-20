31c47f8066bfe157ce7c606782c4b239ab819edf
F2 <916098+F2@users.noreply.github.com>
2026-09-20 14:23:55 +0200

    Added main character sprites

--- screens/main/player/player.gd
+++ screens/main/player/player.gd
@@ -1,6 +1,8 @@
 class_name Player
 extends CharacterBody2D
 
+@onready var player_sprite: AnimatedSprite2D = %PlayerSprite
+
 const PLAYER_WIDTH := 10
 const PLAYER_HEIGHT := 10
 var tile_position: Vector2 = Vector2(0, -1)
@@ -9,6 +11,7 @@ var tile_position: Vector2 = Vector2(0, -1)
 var drill_damage := 1
 var drills_per_second := 4.0
 var last_drill_time := -10.0
+var facing_direction := Vector2i.DOWN
 
 const TILE_MOVEMENT_DELAY := 0.2
 var last_move_time := -10.0
@@ -18,6 +21,7 @@ func _ready() -> void:
 		Util.fatal_error("Player: Level is not set.")
 
 	_update_self_position()
+	_set_player_animation(false)
 	
 
 func _update_self_position() -> void:
@@ -29,23 +33,30 @@ func _update_self_position() -> void:
 
 func _physics_process(_delta: float) -> void:
 	if GlobalState.is_shop_open:
+		_set_player_animation(false)
 		return
 	
 	# If you repeatedly tap a direction, the player will move in that direction
 	# even if TILE_MOVEMENT_DELAY has not passed since last movement.
 	var direction := _get_just_pressed_movement_direction()
+	var is_just_pressed := direction != Vector2i.ZERO
 	if direction == Vector2i.ZERO:
-		if Util.time - last_move_time < TILE_MOVEMENT_DELAY:
-			return
 		direction = _get_movement_direction()
 	if direction == Vector2i.ZERO:
+		_set_player_animation(false)
 		return
 
+	facing_direction = direction
 	var target_tile := Vector2i(tile_position) + direction
 	target_tile.x = clampi(target_tile.x, Level.LEVEL_X_MIN, Level.LEVEL_X_MAX)
 	target_tile.y = clampi(target_tile.y, Level.LEVEL_Y_MIN - 1, Level.LEVEL_Y_MAX)
 
 	var cell: Level.Cell = level.level_data.get(target_tile)
+	_set_player_animation(cell != null && cell.type == Level.CellType.DIRT)
+
+	if not is_just_pressed && Util.time - last_move_time < TILE_MOVEMENT_DELAY:
+		return
+
 	if cell == null || cell.type == Level.CellType.EMPTY:
 		tile_position = Vector2(target_tile)
 		_update_self_position()
@@ -63,6 +74,20 @@ func _physics_process(_delta: float) -> void:
 	last_drill_time = Util.time
 	if level.do_damage(target_tile, drill_damage):
 		last_move_time = Util.time # This ensures that we don't move into the tile we just destroyed until TILE_MOVEMENT_DELAY has passed.
+
+func _set_player_animation(is_drilling: bool) -> void:
+	var direction_name := "down"
+	match facing_direction:
+		Vector2i.LEFT:
+			direction_name = "left"
+		Vector2i.RIGHT:
+			direction_name = "right"
+		Vector2i.UP:
+			direction_name = "up"
+
+	var animation_name := StringName(("drill_" if is_drilling else "idle_") + direction_name)
+	if player_sprite.animation != animation_name || !player_sprite.is_playing():
+		player_sprite.play(animation_name)
 
 func _get_just_pressed_movement_direction() -> Vector2i:
 	var input_direction := Vector2.ZERO
